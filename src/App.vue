ebeb7dde406af07bec46450422b4eca6cb9ffa9c
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-15 21:53:53 +0530

    adjusted screen height

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import NavBar from "./components/NavBar.vue";
     <NavBar />
   </header>
 
-  <div class="h-screen px-2 bg-primary">
+  <div class="h-full min-h-screen px-2 bg-primary">
     <RouterView class="max-w-md mx-auto pt-14" />
   </div>
 </template>
