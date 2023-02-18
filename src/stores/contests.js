7fb1efc65fad2ea35db365e79f6485e4d460055a
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-18 23:48:45 +0530

    fetch contests database

--- src/stores/contests.js
+++ src/stores/contests.js
@@ -1,7 +1,24 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import { supabase } from "../supabase";
 
 export const useContestsStore = defineStore("contests", () => {
-  const contests = ref();
-  return { contests };
+  const contests = ref([]);
+
+  async function getSupabaseData() {
+    try {
+      const { data, error } = await supabase.from("contests").select();
+
+      if (data) {
+        data.forEach((data) => {
+          contests.value.push(data);
+        });
+      }
+      if (error) throw error;
+    } catch (error) {
+      console.warn(error.message);
+    }
+  }
+
+  return { contests, getSupabaseData };
 });
