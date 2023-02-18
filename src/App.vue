97a84191ca3f24d9b72c9873dc585743b73b791e
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-18 23:49:43 +0530

    get database and realtime updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { onMounted } from "vue";
+import { supabase } from "./supabase";
+import { useContestsStore } from "./stores/contests";
 import NavBar from "./components/NavBar.vue";
+
+const contestsStore = useContestsStore();
+
+onMounted(() => {
+  contestsStore.getSupabaseData();
+  subscribeSupabaseData();
+});
+
+const subscribeSupabaseData = () => {
+  supabase
+    .channel("any")
+    .on("postgres_changes", { event: "INSERT", schema: "*" }, (payload) => {
+      contestsStore.contests.push(payload.new);
+    })
+    .on("postgres_changes", { event: "UPDATE", schema: "*" }, (payload) => {
+      const index = contestsStore.contests.findIndex(
+        (item) => item.id === payload.new.id
+      );
+      contestsStore.contests[index] = payload.new;
+    })
+    .on("postgres_changes", { event: "DELETE", schema: "*" }, (payload) => {
+      const index = contestsStore.contests.findIndex(
+        (item) => item.id === payload.old.id
+      );
+      contestsStore.contests.splice(index, 1);
+    })
+
+    .subscribe();
+};
 </script>
 
 <template>
