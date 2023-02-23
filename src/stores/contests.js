9a2c39ab480e1e787155b759cf4ec845aafa75cc
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-23 21:22:02 +0530

    🩹 fixed the loading error on onMounted hook

--- src/stores/contests.js
+++ src/stores/contests.js
@@ -5,7 +5,10 @@ import { supabase } from "../supabase";
 export const useContestsStore = defineStore("contests", () => {
   const contests = ref([]);
 
+  const isLoading = ref(false);
+
   async function getSupabaseData() {
+    isLoading.value = true;
     try {
       const { data, error } = await supabase.from("contests").select();
 
@@ -14,11 +17,12 @@ export const useContestsStore = defineStore("contests", () => {
           contests.value.push(data);
         });
       }
+      isLoading.value = false;
       if (error) throw error;
     } catch (error) {
       console.warn(error.message);
     }
   }
 
-  return { contests, getSupabaseData };
+  return { contests, isLoading, getSupabaseData };
 });
