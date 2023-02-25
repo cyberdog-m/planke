e00ffd47520d853b1c38a5183d1b1c4db793de63
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-25 21:32:41 +0530

    ♻️ open modal on click

--- src/components/EditEventsAdmin.vue
+++ src/components/EditEventsAdmin.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import ConfirmModal from "../components/ConfirmModal.vue";
 import PencilOutlineIcon from "vue-material-design-icons/PencilOutline.vue";
 import DeleteOutlineIcon from "vue-material-design-icons/DeleteOutline.vue";
 
@@ -12,12 +14,24 @@ const router = useRouter();
 function openEditEvent() {
   router.push({ name: "editevent", params: { eventId: props.contest.id } });
 }
+
+// Delete confirmation modal
+const showModal = ref(false);
+
+function closeModal() {
+  showModal.value = false;
+}
 </script>
 
 <template>
   <div
     class="flex items-center justify-between w-full p-3 duration-200 r rounded-xl bg-secondary hover:bg-sechover"
   >
+    <ConfirmModal
+      :show-modal="showModal"
+      :contest="props.contest"
+      @closeModal="closeModal"
+    />
     <h3 class="">{{ props.contest.title }}</h3>
     <div class="flex items-center gap-3">
       <div
@@ -34,6 +48,7 @@ function openEditEvent() {
         <PencilOutlineIcon :size="27" fillColor="#1c1727" />
       </button>
       <button
+        @click="showModal = true"
         class="flex items-center justify-center w-10 h-10 rounded-md bg-rose-400"
       >
         <DeleteOutlineIcon :size="25" fillColor="#1c1727" />
