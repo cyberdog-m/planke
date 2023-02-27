db9e439cdbb91172fcf07c1f8010ceded51dc175
Naveen Mathew <naveenmathew1585@gmail.com>
2023-02-27 22:58:36 +0530

    💄 ui updates

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,26 @@
 <script setup>
-import { computed, ref } from "vue";
-import { RouterLink } from "vue-router";
+import { computed, ref, watch } from "vue";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 import { useUserStore } from "../stores/user";
 import { vOnClickOutside } from "@vueuse/components";
 
+const route = useRoute();
+const router = useRouter();
 const userStore = useUserStore();
 
 const toggleNav = ref(false);
+
 function closeNav() {
   toggleNav.value = false;
+}
+
+// Collapse Navbar on route change
+watch(route, () => {
+  closeNav();
+});
+
+function logout() {
+  userStore.signOutUser();
 }
 
 const avatarUrl = computed(() => {
@@ -23,12 +35,12 @@ const avatarUrl = computed(() => {
     <div
       class="flex items-center justify-between max-w-4xl px-4 mx-auto shadow-sm h-14"
     >
-      <RouterLink to="/">
-        <img
-          src="../assets/union_logo_w-text.png"
-          alt="College Union Logo"
-          class="h-7"
-      /></RouterLink>
+      <img
+        @click="router.push({ name: 'home' })"
+        src="../assets/union_logo_w-text.png"
+        alt="College Union Logo"
+        class="cursor-pointer h-7"
+      />
       <!-- Hamburger Menu -->
       <div class="relative">
         <button
@@ -49,8 +61,8 @@ const avatarUrl = computed(() => {
           v-on-click-outside="closeNav"
           class="absolute mt-2 right-0 z-50 p-3 w-[14rem] bg-secondary outline outline-2 outline-sechover backdrop-blur-md rounded-md"
         >
-          <ul class="flex flex-col gap-1">
-            <li
+          <div class="flex flex-col gap-1">
+            <div
               v-if="userStore.isAuthenticated"
               class="flex flex-col items-center w-full px-2 pt-1 my-1"
             >
@@ -63,35 +75,56 @@ const avatarUrl = computed(() => {
                 {{ userStore.user.full_name }}
               </div>
               <div class="w-full h-[0.1rem] bg-gray-700 my-1"></div>
-            </li>
-            <li
-              class="w-full px-2 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+            </div>
+            <RouterLink
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'home' }"
+              >Home</RouterLink
             >
-              Venue Details
-            </li>
-            <li
-              class="w-full px-2 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+            <RouterLink
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'venue' }"
+              >Venue Details</RouterLink
             >
-              About
-            </li>
-            <li
-              class="w-full px-2 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+            <RouterLink
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'about' }"
+              >About</RouterLink
             >
-              Update Event
-            </li>
-            <li
-              class="w-full px-2 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+            <RouterLink
+              v-if="userStore.isAuthenticated"
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'events-list' }"
+              >Events List</RouterLink
             >
-              Admin Panel
-            </li>
-            <li
-              class="w-full px-2 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+            <RouterLink
+              v-if="!userStore.isAuthenticated"
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'signin' }"
+              >Sign In</RouterLink
+            >
+            <RouterLink
+              v-if="userStore.isAdmin"
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-sechover"
+              :to="{ name: 'admin' }"
+              >Admin</RouterLink
+            >
+            <div
+              v-if="userStore.isAuthenticated"
+              @click="logout"
+              class="w-full px-3 py-2 transition-colors duration-100 ease-in-out rounded-md cursor-pointer hover:bg-sechover"
             >
               Logout
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </nav>
 </template>
+
+<style scoped>
+.router-link-active {
+  @apply font-medium bg-accent/20;
+}
+</style>
