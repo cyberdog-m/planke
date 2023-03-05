d307d0be653f09d7c6f45a3170c920a1dd12be7c
Naveen Mathew <naveenmathew1585@gmail.com>
2023-03-05 17:40:23 +0530

    💄 updated footer links, img optimization

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,31 +1,51 @@
 <script setup>
-import InstagramIcon from "vue-material-design-icons/Instagram.vue";
 import HeartIcon from "vue-material-design-icons/Heart.vue";
 </script>
 <template>
-  <div class="w-full text-white h-[10rem] bg-footer">
+  <div class="w-full text-white min-h-[10rem] bg-footer">
     <div class="max-w-md px-5 pt-5 mx-auto">
       <div class="flex items-center justify-between">
         <div class="flex flex-col">
-          <p>Follow Us</p>
-          <a href="https://instagram.com/element_mcet" class="mt-1 opacity-70">
-            <InstagramIcon :size="30" />
-          </a>
+          <router-link :to="{ name: 'about' }"
+            >About
+            <span class="font-medium text-accent">Planke</span></router-link
+          >
+          <p class="mt-4 opacity-60">Follow Us</p>
+          <div class="flex items-center gap-3 mt-2 opacity-80">
+            <a href="https://instagram.com/element_mcet">
+              <img
+                class="h-8"
+                src="../assets/element-logo.png"
+                alt="Element Association Logo"
+              />
+            </a>
+            <a href="https://instagram.com/mcetians">
+              <img
+                class="h-6"
+                src="../assets/union-logo.png"
+                alt="Malabar Union Logo"
+              />
+            </a>
+          </div>
         </div>
-        <img
-          src="../assets/malabar-logo.png"
-          alt="malabar college logo"
-          class="h-12"
-        />
+        <a href="https://malabarcet.ac.in" target="_blank">
+          <img
+            src="../assets/malabar-logo.png"
+            alt="malabar college logo"
+            class="h-12"
+          />
+        </a>
       </div>
       <!-- Made with Love -->
-      <div class="flex justify-center pt-10 opacity-90">
+      <div
+        class="flex items-center justify-center pt-10 pb-5 text-sm opacity-90"
+      >
         Made with
         <span class="mx-2">
-          <HeartIcon fillColor="#f8312f" />
+          <HeartIcon :size="20" fillColor="#f8312f" />
         </span>
         by
-        <a href="https://github.com/cyberdog-m" class="ml-1 font-medium"
+        <a href="https://github.com/mathaimon" class="ml-1 font-medium"
           >Mathew</a
         >
       </div>
